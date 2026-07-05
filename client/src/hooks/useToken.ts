3c65c7b8d652b04cc1e95daa85be95fec16288ec
Rahul <rahulpedapudi.01@gmail.com>
2026-07-05 18:03:17 +0530

    untract client

--- client/src/hooks/useToken.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { authApi } from "../api/auth";
-
-export const useToken = () => {
-  return useQuery({
-    queryKey: ["telegram"],
-    queryFn: () => authApi.generateLinkToken(),
-  });
-};
